1603d17357ccba339b6e1eba9d6be76ca1b68cd6
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-18 22:10:03 +0900

    再度tweetsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160818125543) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "text",       limit: 65535
+    t.text     "image",      limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
