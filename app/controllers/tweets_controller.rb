9aa02ed515fd5dc28ff813b44544ba85d98f3836
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-23 13:54:32 +0900

    サインインしていないユーザーがindexアクションにリダイレクトするように、move_to_indexメソッドを定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,4 +16,8 @@ class TweetsController < ApplicationController
     params.permit(:name, :image, :text)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
