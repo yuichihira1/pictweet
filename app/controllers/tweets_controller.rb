be0248132e50b64912a00c364752cdc0e284962e
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-23 15:35:37 +0900

    記述ミスを修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(name: tweet_params[:name], image: tweets_params[:image], tweets_params[:text], user_id: current_user.id)
+     Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
