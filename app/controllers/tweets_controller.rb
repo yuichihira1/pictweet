f8896d9170ad15bbed530d7902ad83cf270136cf
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-26 20:04:21 +0900

    createアクションが動いた時にname情報をカラムに保存しないようにコントローラーから記述を削除

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,7 +11,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-     Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+     Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
