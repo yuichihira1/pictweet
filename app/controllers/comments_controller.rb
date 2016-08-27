394abea245027d7b35fb859410c2f95bbe7689be
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-27 18:21:46 +0900

    commentsコントローラーにツイート詳細ページへのリダイレクトを記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(text: params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
+    redirect_to "/tweets/#{@comment.tweet.id}"
 
     private
     def comment_params
