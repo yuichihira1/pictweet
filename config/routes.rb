7a882295e4c954d03f25d929f2669f4e43284414
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-27 17:49:20 +0900

    tweetsへのルーティングの中にcommentsのcreateアクションのルーティングをネストさせた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for: users
+  devise_for :users
   root 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    recources :comments, only: [:create]
+  end
   resources :users, only: [:show]
 end
