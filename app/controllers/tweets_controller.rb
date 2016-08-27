e95077360ca463e9792fe3fd2d98265537ec6d90
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-27 13:57:57 +0900

    updateメソッドを定義してupdateアクションを実装した

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -25,6 +25,13 @@ def destroy
       @tweet = Tweet.find(params[:id])
     end
 
+    def update
+       @tweet = Tweet.find(params[:id])
+       if tweet.user_id == current_user.id
+        tweet.update(tweet_params)
+      end
+    end
+
   private
   def tweet_params
     params.permit(:image, :text)
