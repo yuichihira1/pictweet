f17df8210c7424e8edab474ef38e91f981838ea7
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-27 17:25:15 +0900

    commentモデルにtweetsテーブル、commentsテーブルへのアソシエーションを記述

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  belongs_to :tweet_id  #tweetテーブルへのアソシエーション
+  belongs_to :user #usersテーブルへのアソシエーション
   def change
     create_table :comments do |t|
       t.integer :user_id
