8cf253c5c8f8b37d8c6dbcee6e1db02061d67f51
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-27 11:42:41 +0900

    destroyメソッドを正しく記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,13 +14,12 @@ class TweetsController < ApplicationController
      Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
-  def deatroy
-    tweet = Tweet.find(params[:id])
-    if tweet.user_id == curret_user.id
-      tweet.destroy
-  end
-end
-
+def destroy
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.destroy
+      end
+    end
 
   private
   def tweet_params
