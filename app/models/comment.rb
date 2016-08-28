be55dc96c15532718dbae631f533fd6a7790e895
yuichihira1 <yhira0514@yahoo.co.jp>
2016-08-28 15:11:07 +0900

    モデルに誤ってテーブル情報を記述したので削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :tweet #tweetテーブルへのアソシエーション
-  belongs_to :user #usersテーブルへのアソシエーション
-  def change
-    create_table :comments do |t|
-      t.integer :user_id
-      t.integer :tweet_id
-      t.text :text
-      t.timestamps
-    end
+  belongs_to :tweet
+  belongs_to :user
 end
